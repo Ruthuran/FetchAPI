20d5aa21fa338b7a938291ab72407809b6e9aa7c
Ruthuran Muralirajan <ruthuranruthuran@gmail.com>
2025-01-24 13:27:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import Fetch from './fetch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Fetch API</h1>
+      <h2>User Information</h2>
+      <Fetch/>
+    </div>
+  );
+}
+
+export default App;
